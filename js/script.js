776b7d26239cedba80010a36916a80548d33a112
Nezz26 <103019177+Nezz26@users.noreply.github.com>
2024-07-27 12:16:18 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,26 +1,29 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 0;
+showSlides();
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
+function showSlides() {
     let i;
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}    
-    if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";  
     }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex-1].style.display = "block";  
     dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 3000); // Ubah gambar setiap 3 detik
+}
+
+function plusSlides(n) {
+    slideIndex += n - 1;
+    showSlides();
+}
+
+function currentSlide(n) {
+    slideIndex = n - 1;
+    showSlides();
 }
